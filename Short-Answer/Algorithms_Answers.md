db31eeff6a55d6860b0ced0f43ce7ce02497a840
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-07-24 13:50:38 -0400

    met mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -3,13 +3,18 @@
 ## Exercise I
 
 a)
-
+our loop only runs while our condition is true and with the way our condition is set up, we get closer and closer to it with each iteration
 
 b)
-
+there are two loops and one of them is nested, therefore squaring the work
 
 c)
-
+The recursion relies on the input, it determines how many computations will happen until we reach our base case
 ## Exercise II
-
+initiate my function to take in parameter of f (f = floor egg will be dropped from)
+  within the function, have two if statements
+  a variable to check if egg has been broken
+  two conditions depending if egg has been broken
+    1, if egg has not been broken, run the function again only this time f will be one more than the previous
+    2, if egg is broken, return f because that is the floor it breaks from
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,19 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+     #init variable  with count to start at 0 since there have been no instances of "th"
+    #check length
+    #if len(word) < 2, return count (should be 0)
+    #if len(word) > 2, proceed with checking the word for "th"
+    #check first two indexes if they equal "th"
+        #if current two indexes include "th"(['t', 'h']), increase count by 1
+        #if not, return the funcion again only this time with the first two indexes of word sliced off
+    count = 0
+    if len(word) < 2:
+        return count
+    else:
+        if word[:2] == 'th':
+            count += 1
+    return count + count_th(word[1:])
+
+print(count_th('thth'))
